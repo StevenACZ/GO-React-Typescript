ad5c95da2924219f87c46b465d8f0bf62a8c84e2
stevenacz <scoailazaa@gmail.com>
2021-04-15 18:52:44 -0500

    tipos basicos primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 // React
 import React from 'react';
+import TiposBasicos from './typescript/TiposBasicos';
 
 interface Props {}
 
 const App: React.FC<Props> = () => {
-  return <div>Introduccion a TS-React</div>;
+  return (
+    <div className="mt-2">
+      <h1>Introduccion a TS-React</h1>
+      <hr />
+      <TiposBasicos />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,14 @@
+// React
+import React from 'react';
+
+interface Props {}
+
+const TiposBasicos: React.FC<Props> = () => {
+  return (
+    <>
+      <h3>Tipos basicos</h3>
+    </>
+  );
+};
+
+export default TiposBasicos;
