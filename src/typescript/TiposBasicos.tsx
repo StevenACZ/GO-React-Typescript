c1494ef9c69d774766113a299af3aedb1ce5bcda
stevenacz <scoailazaa@gmail.com>
2021-04-15 19:02:59 -0500

    Tipos basicos

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -4,9 +4,20 @@ import React from 'react';
 interface Props {}
 
 const TiposBasicos: React.FC<Props> = () => {
+  const nombre: string = 'Fernando';
+  const edad: number = 35;
+  const estaActivo: boolean = true;
+
+  const poderes: string[] = [];
+
+  poderes.push('hola');
+
   return (
     <>
       <h3>Tipos basicos</h3>
+      {nombre}, {edad}, {estaActivo ? 'true' : 'false'}
+      <br />
+      {poderes.join(', ')}
     </>
   );
 };
