61a3569a058b9d67afa450263975055d8412ea7d
stevenacz <scoailazaa@gmail.com>
2021-04-16 13:30:17 -0500

    AuthReducer

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,69 @@
 // React
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+
+interface AuthState {
+  success: boolean;
+  user: User;
+}
+
+interface User {
+  nickName: null | string;
+  name: null | string;
+  token: null | string;
+}
+
+const initialState: AuthState = {
+  success: true,
+  user: {
+    nickName: null,
+    name: null,
+    token: null,
+  },
+};
+
+type AuthAction = {
+  type: 'logout';
+};
+
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+  switch (action.type) {
+    case 'logout':
+      return {
+        success: false,
+        user: {
+          nickName: null,
+          name: null,
+          token: null,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
 
 interface Props {}
 
 const Login: React.FC<Props> = () => {
+  const [{ success }, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: 'logout' });
+    }, 1500);
+  }, []);
+
+  if (success) {
+    return (
+      <>
+        <div className="alert alert-info">Validando...</div>
+      </>
+    );
+  }
+
   return (
     <>
       <h3>Login</h3>
-      <div className="alert alert-info">Validando...</div>
       <div className="alert alert-danger">No autenticado...</div>
       <div className="alert alert-success">Autenticado...</div>
       <button className="btn btn-primary">Login</button>{' '}
