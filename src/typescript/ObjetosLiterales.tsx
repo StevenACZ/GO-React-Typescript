e5a4c590564eff57b3f30f8c6127f2fe2a8e6773
stevenacz <scoailazaa@gmail.com>
2021-04-15 19:11:05 -0500

    added

---
+++ src/typescript/ObjetosLiterales.tsx
@@ -0,0 +1,37 @@
+// React
+import React from 'react';
+
+interface Props {}
+
+interface Persona {
+  nombreCompleto: string;
+  nombre: string;
+  edad: number;
+  direccion: {
+    pais: string;
+    casaNo: number;
+  };
+}
+
+const ObjetosLiterales: React.FC<Props> = () => {
+  const persona: Persona = {
+    nombreCompleto: 'Fernando Perez',
+    nombre: 'Fernando',
+    edad: 35,
+    direccion: {
+      pais: 'Canada',
+      casaNo: 134,
+    },
+  };
+
+  return (
+    <>
+      <h3>Objetos Literales</h3>
+      <code>
+        <pre>{JSON.stringify(persona, null, 2)}</pre>
+      </code>
+    </>
+  );
+};
+
+export default ObjetosLiterales;
