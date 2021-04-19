a544ae6a37e3d5733e89613c6a3a6df06e38b8d6
stevenacz <scoailazaa@gmail.com>
2021-04-19 13:00:59 -0500

    getUsers added

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,12 +1,35 @@
 // React
-import React from 'react';
+import React, { useEffect } from 'react';
+
+// Axios
+import reqResApi from '../api/reqRes';
 
 interface Props {}
 
 const Usuarios: React.FC<Props> = () => {
+  const getUsers = async () => {
+    const {
+      data: { data },
+    } = await reqResApi.get('/users');
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <>
       <h3>Usuarios</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Nombre</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+      </table>
     </>
   );
 };
